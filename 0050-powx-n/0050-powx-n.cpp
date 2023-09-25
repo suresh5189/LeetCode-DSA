d411f4d5a0f73577631ece15e7681e7ac28c7750
Suresh Suthar <110899811+suresh5189@users.noreply.github.com>
2023-09-25 18:01:52 +0530

    Time: 3 ms (42.54%), Space: 6.8 MB (5.40%) - LeetHub

---
+++ 0050-powx-n/0050-powx-n.cpp
@@ -0,0 +1,10 @@
+#include <bits/stdc++.h>
+using namespace std;
+class Solution {
+public:
+    double myPow(double x, int n) {
+    double answer=0;
+        answer= pow(x,n);
+        return answer;
+    }
+};
